b8921127164b701168cc17898e70079bd8cef907
venysssssssssss <shablaur7@hotmail.com>
2024-05-14 15:08:34 -0300

    feat: Add Rust CRUD API with PostgreSQL integration

--- hello.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let input = "Hello, World!";
-    
-    for c in input.chars() {
-        let ascii_value = c as u8;
-        println!("Character: {}, ASCII Value: {}", c, ascii_value);
-    }
-}
